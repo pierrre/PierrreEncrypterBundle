e73fd9f941965ba746659d7118108f6235b26ca9
Pierre Durand <pierredurand@gmail.com>
2012-01-30 21:47:45 +0100

    Add unit test for bundle class This project is now fully unit tested

---
+++ Tests/PierrreEncrypterBundleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pierrre\EncrypterBundle\Tests;
+
+use Pierrre\EncrypterBundle\PierrreEncrypterBundle;
+
+class PierrreEncrypterBundleTest extends \PHPUnit_Framework_TestCase{
+	/**
+	 * @covers Pierrre\EncrypterBundle\PierrreEncrypterBundle
+	 */
+	public function testClass(){
+		$bundle = new PierrreEncrypterBundle();
+	}
+}
